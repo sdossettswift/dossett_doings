ebfe32dbccd238e3f0a423359b13dc00f601ed51
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-26 22:34:31 -0500

    recipes and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827031513) do
+ActiveRecord::Schema.define(version: 20160827032207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20160827031513) do
   create_table "directions", force: :cascade do |t|
     t.text     "step"
     t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.integer  "quantity"
+    t.string   "unit"
+    t.string   "ingredient"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
